31cb92b2bb8b334df4fc089c9cee07afaf134479
samirahmad5445 <samirahmad5445@gmail.com>
2023-10-26 00:17:28 +0300

    init commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          400: "#4d13d1",
+        },
+        neutral: {
+          100: "#ffffff",
+          200: "#fbf8f3",
+          300: "#e5ddfd",
+          400: "#dadada",
+          500: "#a9a9a9",
+          600: "#4f4f4f",
+          900: "#000000",
+        },
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
