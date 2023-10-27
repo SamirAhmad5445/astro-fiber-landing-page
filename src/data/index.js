f3b67f0ae665682e9753a0303138af486093c2d0
samirahmad5445 <samirahmad5445@gmail.com>
2023-10-27 18:32:20 +0200

    add main content

--- src/data/index.js
+++ src/data/index.js
@@ -3,3 +3,48 @@ export const rating = {
   stars: 5,
   reviews: 243,
 };
+
+export const features = [
+  {
+    icon: "time",
+    name: "Build in minutes",
+    description:
+      "With a selection of premade templates, you can build out a portfolio in less than 10 minutes.",
+  },
+
+  {
+    icon: "code",
+    name: "Add custom CSS",
+    description:
+      "Customize your personal portfolio even more with the ability to add your own custom CSS styles.",
+  },
+  {
+    icon: "responsive",
+    name: "Responsive",
+    description:
+      "All Fiber templates are fully responsive to ensure the experience is seamless across all devices.",
+  },
+];
+
+export const testimonials = [
+  {
+    username: "Sarah Andrews",
+    profileImage: "/assets/user-avatar-1.svg",
+    revenue: "$100k",
+    text: "Setting up my portfolio with Fiber took no more than 10 minutes. Since then, my portfolio has attracted a lot of clients and made me more than $100k.",
+  },
+
+  {
+    username: "Mathew Higgins",
+    profileImage: "/assets/user-avatar-2.svg",
+    revenue: "$20k",
+    text: "I have been getting A LOT of leads ever since I used Fiber’s premade templates, now I just need to work on my case studies and I’ll be ready to go!",
+  },
+
+  {
+    username: "Janice Dave",
+    profileImage: "/assets/user-avatar-3.svg",
+    revenue: "$30k",
+    text: "I only just started freelancing this year and I have already made more than I ever made in my full-time job. The templates are just so amazing.",
+  },
+];
